3075ff620e6b4220de128b57b8d52e2d1aeb298e
Carter Himmel <carter@elhnet.net>
2020-05-18 21:05:56 -0600

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# @fyko/botmart
+a package to aide in fetching the latest sales of botmart products
+
+## todo
+- [ ] type guarding for lifetime & renewal products
--- test/Util.ts
+++ test/Util.ts
@@ -1,6 +1,31 @@
 import ava from 'ava';
-import util from '../dist';
+import { Products, fetchData, craftURL } from '../dist';
 
-ava('sample test', (test): void => {
-	test.is(util(), 'bar');
+ava('every product returns a number', (test): void => {
+	const products = Object.values(Products);
+	test.plan(products.length);
+
+	for (const product of products) {
+		test.is(typeof product, 'number');
+	}
+});
+
+ava('every product title is in full-caps', (test): void => {
+	const products = Object.keys(Products);
+	test.plan(products.length);
+
+	for (const product of products) {
+		test.is(product, product.toUpperCase());
+	}
+});
+
+ava('every product request returns an array', async (test): Promise<void> => {
+	const products = Object.values(Products);
+	test.plan(products.length);
+
+	for (const product of products) {
+		const url = craftURL(product);
+		const data = await fetchData(url);
+		test.true(Array.isArray(data));
+	}
 });
